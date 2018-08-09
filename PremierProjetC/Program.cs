58610e698f8cb269a05121fa9a65ffb56eb54b84
Eternal-Ben <41895405+Eternal-Ben@users.noreply.github.com>
2018-08-09 03:28:28 +0200

    Update Program.cs

--- PremierProjetC/Program.cs
+++ PremierProjetC/Program.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO; //permet l'utilisation de recherche de fichier dans le system lors de l'apl de fonction List<FileInfo> Files { get; set; }
 
-namespace PremierProjetC
+namespace PremierProjetC //renommer le nom du Proj suivant le projet donné
 {
-    class Program
+    class Program // renommer la classe Program par le nom de la classe principal qui identifira le nom du programme à creer et qui servira de point d'entrée via le Main. 
     {
         static void Main(string[] args)
         {
