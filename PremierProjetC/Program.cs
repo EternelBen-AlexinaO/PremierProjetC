ae0f677eb239e8032d09432fe1626ede01cc95d8
Eternal-Ben <41895405+Eternal-Ben@users.noreply.github.com>
2018-08-09 03:25:06 +0200

    Update Program.cs

--- PremierProjetC/Program.cs
+++ PremierProjetC/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; //permet l'utilisation de recherche de fichier dans le system lors de l'apl de fonction List<FileInfo> Files { get; set; }
 
 namespace PremierProjetC
 {
