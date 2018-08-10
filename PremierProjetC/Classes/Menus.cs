94a8048ebbd2b8735b32bb34d618b487dad4754b
AlexinaO <alexina.orsoni@gmail.com>
2018-08-10 11:12:33 +0200

    Création de la méthode pour afficher le Menu de Gestion Commerciale

--- PremierProjetC/Classes/Menus.cs
+++ PremierProjetC/Classes/Menus.cs
@@ -19,6 +19,18 @@ namespace PremierProjetC.Classes
             Esthetisme.MiseEnFormeTexte("Mot de Passe: ", centre: false);
             Console.ReadLine();
         }
-        
-     }
+
+        public static void MenuGestionCommerciale()
+        {
+            Console.Clear();
+            Esthetisme.MiseEnFormeTexte("APPLICATION METIER DE BO VOYAGE\n\n", ConsoleColor.DarkCyan, centre: true);
+            Esthetisme.MiseEnFormeTexte("Connexion réussie\n\n", ConsoleColor.Red, centre: false);
+            Esthetisme.MiseEnFormeTexte("MENU\n\n", centre: false);
+            Esthetisme.MiseEnFormeTexte("1. Gestion des voyages\n\n", centre: false);
+            Esthetisme.MiseEnFormeTexte("2. Gestion des clients\n\n", centre: false);
+
+
+        }
+
+    }
 }
