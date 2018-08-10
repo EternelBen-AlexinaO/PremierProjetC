d45e86bd1f8a8d169e0705e96a0441c419c8a34d
AlexinaO <alexina.orsoni@gmail.com>
2018-08-10 12:05:57 +0200

    Ajout des paramètres dans la classe Adresse

--- PremierProjetC/Classes/Adresse.cs
+++ PremierProjetC/Classes/Adresse.cs
@@ -8,6 +8,12 @@ namespace PremierProjetC.Classes
 {
     public class Adresse
     {
+        public int NumeroVoie { get; set; }
+        public string NomVoie { get; set; }
+        public int CodePostal { get; set; }
+        public string Ville { get; set; }
+        public string Pays { get; set; }
+
 
     }
 }
