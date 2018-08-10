2f874989157406459fac340a19673253c601c687
AlexinaO <alexina.orsoni@gmail.com>
2018-08-10 10:03:28 +0200

    Revert "Création de la méthode MiseEnFormeTexte pour choisir une couleur pour un texte et le centrer ou non"
    This reverts commit 0360f00cba632da2a4bc361ea20bc292906706c7.

--- PremierProjetC/Classes/Esthetisme.cs
+++ PremierProjetC/Classes/Esthetisme.cs
@@ -8,20 +8,5 @@ namespace PremierProjetC.Classes
 {
     public class Esthetisme
     {
-    //Methode pour mettre le texte de la couleur voulue et centrer ou non le texte
-    public static void MiseEnFormeTexte(string message, ConsoleColor couleur=ConsoleColor.Gray, bool centre=false)
-        {
-            Console.ForegroundColor = couleur;
-            if (centre)
-            {
-                int nbEspaces = (Console.WindowWidth - message.Length) / 2;
-                Console.SetCursorPosition(nbEspaces, Console.CursorTop);
-            }
-            Console.WriteLine(message);
-            Console.ResetColor();
-        }
-
-
-
     }
 }
