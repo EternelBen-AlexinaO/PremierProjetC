e4f308b589f76b3f0d9bfe992e0b80eb38004b6c
AlexinaO <alexina.orsoni@gmail.com>
2018-08-10 14:27:54 +0200

    retrait du type adresse et remplacement par les données utiles (NumeroVoie, NomVoie, Ville, Pays)

--- PremierProjetC/Classes/Participant.cs
+++ PremierProjetC/Classes/Participant.cs
@@ -12,7 +12,13 @@ namespace PremierProjetC.Classes
         public string Nom { get; set; }
         public string Prenom { get; set; }
         public DateTime DateNaissance { get; set; }
-        public Adresse AdresseParticipant { get; set; }
+        //public Adresse AdresseParticipant { get; set; } n'arrivant pas à l'utiliser, nous n'utilisons pas ce type
+        public int NumeroVoie { get; set; }
+        public string NomVoie { get; set; }
+        public string Ville { get; set; }
+        public string Pays { get; set; }
+
+        
 
     }
 }
