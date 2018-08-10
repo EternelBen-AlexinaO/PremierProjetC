0d0cc6633be19e0e2eca686f31e4237d77bfb5f8
Admin <lst.thierry@gmail.com>
2018-08-10 15:53:06 +0200

    Création des menu gestion voyage et gestion client

--- PremierProjetC/Classes/Menus.cs
+++ PremierProjetC/Classes/Menus.cs
@@ -34,7 +34,7 @@ namespace PremierProjetC.Classes
             return Console.ReadLine();
         }
 
-        public static string MenuGestionVoyages()
+        public static void MenuGestionVoyages()
         {
             Console.Clear();
             Esthetisme.MiseEnFormeTexte("APPLICATION METIER DE BO VOYAGE\n\n", ConsoleColor.DarkCyan, centre: true);
@@ -48,26 +48,92 @@ namespace PremierProjetC.Classes
             Esthetisme.MiseEnFormeTexte("Q. Quitter\n\n", centre: false);
             Esthetisme.MiseEnFormeTexte("Faites votre choix:", centre: false);
 
-            return Console.ReadLine();
+            var choix2 = Console.ReadLine();
+
+            switch (choix2)
+            {
+                case "1":
+                    ListerLesDossierEnAttente();
+                    break;
+
+                case "2":
+                    ListerLesDossiersEnCours();
+                    break;
+
+                case "3":
+                    ListerLesDossiersRefusé();
+                    break;
+
+                case "4":
+                    ListerLesDossiersAcceptés();
+                    break;
+
+                case "5":
+                    ListeDesVoyages();
+                    break;
+
+                case "r":
+                case "R":
+                    MenuGestionCommerciale();
+                    break;
+
+                case "q":
+                case "Q":
+                    break;
+                default:
+                    Classes.Esthetisme.MiseEnFormeTexte("Choix invalide, l'application va fermer", ConsoleColor.Red, centre: false);
+                    break;
+            }
+            Console.ReadKey();
         }
 
-        public static string MenuGestionClients()
+        public static void MenuGestionClients()
         {
             Console.Clear();
             Esthetisme.MiseEnFormeTexte("APPLICATION METIER DE BO VOYAGE\n\n", ConsoleColor.DarkCyan, centre: true);
-            Esthetisme.MiseEnFormeTexte("GESTION DES CLIENTS\n",centre: false);
+            Esthetisme.MiseEnFormeTexte("GESTION DES CLIENTS\n", centre: false);
             Esthetisme.MiseEnFormeTexte("1. Voir la liste des clients", centre: false);
             Esthetisme.MiseEnFormeTexte("2. Rechercher un client", centre: false);
             Esthetisme.MiseEnFormeTexte("3. Créer un client", centre: false);
             Esthetisme.MiseEnFormeTexte("4. Supprimer un client", centre: false);
-            Esthetisme.MiseEnFormeTexte("R. Revenir au menu précédent", centre: false);
+            Esthetisme.MiseEnFormeTexte("R. Revenir au menu gestion commerciale", centre: false);
             Esthetisme.MiseEnFormeTexte("Q. Quitter\n\n", centre: false);
             Esthetisme.MiseEnFormeTexte("Faites votre choix:", centre: false);
 
-            return Console.ReadLine();
+            var choix3 = Console.ReadLine();
+
+            switch (choix3)
+            {
+                case "1":
+                    VoirListeDesClients();
+                    break;
+
+                case "2":
+                    RechercherUnClient();
+                    break;
+
+                case "3":
+                    CreerUnClient();
+                    break;
+
+                case "4":
+                    SupprimerClient();
+                    break;
+
+                case "r":
+                case "R":
+                    MenuGestionCommerciale();
+                    break;
+
+                case "q":
+                case "Q":
+                    break;
+                default:
+                    Classes.Esthetisme.MiseEnFormeTexte("Choix invalide, l'application va fermer", ConsoleColor.Red, centre: false);
+                    break;
+            }
+            Console.ReadKey();
+
         }
-
-
-
     }
 }
