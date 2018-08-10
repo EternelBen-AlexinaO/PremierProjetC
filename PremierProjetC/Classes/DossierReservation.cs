d837ed4e00dc2736b559d652b8f71c51e8ead05d
AlexinaO <alexina.orsoni@gmail.com>
2018-08-10 21:50:47 +0200

    Ajout des paramètres, relations et méthodes pour la Classe DossierReservation

--- PremierProjetC/Classes/DossierReservation.cs
+++ PremierProjetC/Classes/DossierReservation.cs
@@ -8,5 +8,13 @@ namespace PremierProjetC.Classes
 {
     public sealed class DossierReservation
     {
+        public int NumeroDossier { get; set; }
+        public int NbParticipants { get; set; }
+
+        public List<Commercial>Commeerciaux { get; set; }
+        public List<Participant>Participants { get; set; }
+        public List<Promotion>Promotions { get; set; }
+
+        public double CalculerPrixTotal() //Methode qui devra calculer le prix total en prenant en compte les tarifs promotionnels suivant l'âge des participants
     }
 }
